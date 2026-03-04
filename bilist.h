6095a4ba200284dd4dda23c3f6f02cd6339e564d
ryouta25 <avp22.07@mail.ru>
2026-03-05 00:04:16 +0300

    Added functions add_back and add_front

--- bilist.h
+++ bilist.h
@@ -9,3 +9,36 @@ struct BiList {
     BiList<T>* next;
     BiList<T>* prev;
 };
+
+template< class T >
+BiList<T>* add_front(BiList<T>* head, const T& val) {
+    BiList<T>* newNode = new BiList<T>;
+    newNode->data = val; // T должен поддерживать оператор присваивания
+    newNode->prev = nullptr;
+    newNode->next = head;
+
+    if (head) {
+        head->prev = newNode;
+    }
+    return newNode;
+}
+
+template< class T >
+BiList<T>* add_back(BiList<T>* head, const T& val) {
+    BiList<T>* newNode = new BiList<T>;
+    newNode->data = val; // T должен поддерживать оператор присваивания
+    newNode->next = nullptr;
+
+    if (!head) {
+        newNode->prev = nullptr;
+        return newNode;
+    }
+
+    BiList<T>* current = head;
+    while (current->next) current = current->next;
+
+    current->next = newNode;
+    newNode->prev = current;
+
+    return head;
+}
